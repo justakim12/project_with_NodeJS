dd6b9090f293752b9fc05a85873f5975ef8cba84
JustaKim <rlaruddlf711@naver.com>
2020-12-27 23:14:51 +0900

    Connect to MongoDB

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dotenv = require('dotenv')
+dotenv.config()
+
+const password = process.env.DB_PASSWORD
+
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://david-kim:${password}@boilerplate.p2j4o.mongodb.net/<dbname>?retryWrites=true&w=majority`, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
