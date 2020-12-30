b3fd5e253ddf4ceb15ae488ade598ef87078460c
JustaKim <rlaruddlf711@naver.com>
2020-12-30 21:02:16 +0900

    Add login page components

--- boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
     return (
-        <div>
-            LoginPage
-        </div>
+    <div className="login-page">
+        <form style={{ display: 'flex', flexDirection: 'column'}}>
+            <label>Email</label>
+            <input type="email" value={Email} onChange={onEmailHandler} />
+            <label>Password</label>
+            <input type="password" value={Password} onChange={onPasswordHandler} />
+
+            <br />
+            <button>
+                Login
+            </button>
+
+            
+
+        </form>
+    </div> 
     )
 }
 
